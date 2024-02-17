bbcb04a333bbb3c7bfc83a07611991c8b67e8ad0
surabhiwaingankar <surabhiwaingankar@gmail.com>
2024-02-18 03:42:17 +0530

    added agents

--- hackniche/lib/Python/Server1/langchain_agent.py
+++ hackniche/lib/Python/Server1/langchain_agent.py
@@ -1,32 +1,31 @@
+from langchain_community.llms import OpenAI
 from langchain_community.chat_models import ChatOpenAI
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from dotenv import load_dotenv
-load_dotenv()
+from langchain.agents import Tool
+from langchain.prompts import PromptTemplate
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
 from langchain.chains import LLMRequestsChain,LLMChain
-from langchain.prompts.chat import (
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
 
 
- 
-def scraping_doxumentation(query:str):
-    
+load_dotenv()
+
+def scraping_documentation(query:str):
+
     scarping_llm=ChatOpenAI(model_name="gpt-4-turbo-preview")
     template="""
     Extract the documentation of langchain for the following query :
     {query}
     """
-    prompt = ChatPromptTemplate.from_template(template=template)
-    messages=prompt.format_messages(
-        input=query,
-        
+    prompt =PromptTemplate(
+    input_variables=["query"],
+    template=template,
     )
-    chain=LLMRequestsChain(llm_chain=LLMChain(llm=scarping_llm,prompt=messages))
+    chain=LLMRequestsChain(llm_chain=LLMChain(llm=scarping_llm,prompt=prompt))
     inputs={
         "query":query,
         "url":f"https://python.langchain.com/docs/get_started/introduction+"+query.replace(" ","+")   
     }
     documentation=chain.run(inputs)
     return documentation
-documentation=scraping_doxumentation("Create a chain in langchain")
