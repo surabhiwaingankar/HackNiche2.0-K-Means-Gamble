2b11a5a3162b8ec11275dd806382528a3f6968d3
Saurav Haldar <143389228+SauravHaldar04@users.noreply.github.com>
2024-02-17 10:25:29 +0530

    Adding Landing Page Background

---
+++ hackniche/lib/screens/landing_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatefulWidget {
+  const LandingPage({super.key});
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(builder: ((context, constraints) {
+      return Container(
+        child: SingleChildScrollView(
+          child: Stack(
+            children: [
+              Container(
+                width: constraints.maxWidth,
+                height: constraints.maxHeight,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/CosmicHero.png'),
+                      fit: BoxFit.cover),    
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+    }));
+  }
+}
